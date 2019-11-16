8a6c1ab4ff0d49844c4882d20829de9708e9ba46
Aphelion <Aphelion01237@gmail.com>
2019-11-15 20:42:35 -0800

    working friend finder

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+  {
+    name: "Summer Smith",
+    photo: "https://media.giphy.com/media/S4HTo1uhqDplfhoaSk/giphy.gif",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Morty Smith",
+    photo: "https://media.giphy.com/media/v9lHVupaHH5Dy/giphy.gif",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Rick Sanchez",
+    photo: "https://media.giphy.com/media/YSJDkuVwpjOVO/giphy.gif",
+    scores: [
+      "5",
+      "3",
+      "5",
+      "1",
+      "2",
+      "5",
+      "5",
+      "3",
+      "1",
+      "1"
+    ]
+  }
+
+];
+
+module.exports = friends;
